137abf196f079caca7769099411608d6789b33d7
Ardintsev Sergey <archunter13@yandex.ru>
2021-10-10 19:31:42 +0300

    middleware err fix

--- 02_first_project/src/shared/Header.jsx
+++ 02_first_project/src/shared/Header.jsx
@@ -6,8 +6,8 @@ function HeaderComponent() {
   console.log(styles, styles.example);
   return (
     <header>
-      <h1 className={styles.example}>Reddit for our own</h1>
-      <h2>Hello React</h2>
+      <h1 className={styles.hello}>Hello React</h1>
+      <h2 className={styles.example}>Reddit for our own</h2>      
     </header>
   )
 }
