7077b40ad7d254aac2edf4c8c1b13698e01aff41
Ardintsev Sergey <archunter1369@gmail.com>
2022-07-19 19:49:26 +0300

    other branches deleted

---
+++ 05_hooks/src/shared/Icons/Icon.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import React from 'react'
+import BlockIcon from "./BlockIcon";
+import CommentIcon from "./CommentIcon";
+import ReportIcon from "./ReportIcon";
+import ShareIcon from "./ShareIcon";
+import SaveIcon from "./SaveIcon";
+import styles from './icon.css';
+
+export enum IconsName {
+    block = 'block',
+    comment = 'comment',
+    report = 'report',
+    share = 'share',
+    save = 'save',
+}
+
+export interface IIConsDefault {
+    className?: string,
+    type?: IconsName,
+    size?: any,
+    animated?: string,
+}
+
+const Icon = (props: IIConsDefault) => {
+    const [defSize, setDefSize] = useState(14);
+    useEffect(() => {
+        if (props.size) {
+            setDefSize(props.size)
+        };        
+    }, []);
+
+    const PossibleIcons = () => {
+        switch (props.type) {
+            case (IconsName.block ) : return <BlockIcon size={defSize} />
+            case (IconsName.comment) : return <CommentIcon size={defSize} />
+            case (IconsName.report) : return <ReportIcon size={defSize} />
+            case (IconsName.share) : return <ShareIcon size={defSize} />
+            case (IconsName.save) : return <SaveIcon size={defSize} />
+            default : return <BlockIcon size={defSize} />
+        }
+    }
+
+    return (
+        <div style={{marginRight: '5px'}}
+            className={props.className ? props.className : styles.default_icon}>
+            <PossibleIcons />
+        </div>
+    )
+}
+
+export default Icon;
--- 09_portal/src/shared/Post/PostHeader/PostHeader.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export function PostHeader() {
-  return (
-    <h2>Следует отметить, что новая модель организационной деятельности поможет</h2>
-
-  )
-}
