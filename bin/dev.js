a1aa2b02f281f4516bccba875afe289f3d5e611a
Ardintsev Sergey <archunter1369@gmail.com>
2022-08-23 20:44:29 +0300

    config edited

--- bin/dev.js
+++ bin/dev.js
@@ -1,52 +1,71 @@
-const webpack = require('webpack');
-const [webpackClientConfig, webpackServerConfig] = require('../webpack.config');
-const nodemon = require('nodemon');
-const path = require('path');
-const webpackDevMiddleWare = require('webpack-dev-middleware');
-const webpackHotMiddleWare = require('webpack-hot-middleware');
-const express = require('express');
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack')
+const [webpackClientConfig, webpackServerConfig] = require('../webpack.config')
+const nodemon = require('nodemon')
+const path = require('path')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const express = require('express')
+const cors = require('cors')
 
-const hmrServer = express();
-const clientCompiler = webpack(webpackClientConfig);
+const compiler = webpack(webpackServerConfig)
+const clientCompiler = webpack(webpackClientConfig)
 
-hmrServer.use(webpackDevMiddleWare(clientCompiler, {
-  publicPath: webpackClientConfig.output.publicPath,
-  serverSideRender: true,
-  noInfo: true,
-  watchOptions: {
-    ignore: /dist/,
-  },
-  writeToDisk: true,
-  stats: 'errors-only',
-}));
+const hmrServer = express()
 
-hmrServer.use(webpackHotMiddleWare(clientCompiler, {
-  path: '/static/__webpack_hmr',
-}));
+hmrServer.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true)
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  }
+}))
+
+hmrServer.use(
+  webpackDevMiddleware(clientCompiler, {
+    publicPath: '//localhost:3001/static/',
+    serverSideRender: true,
+    noInfo: true,
+    watchOptions: {
+      ignore: /dist/,
+    },
+    writeToDisk: true,
+    stats: 'errors-only',
+  })
+)
+
+hmrServer.use(
+  webpackHotMiddleware(clientCompiler, {
+    path: '/static/__webpack_hmr',
+  })
+)
 
 hmrServer.listen(3001, () => {
-  console.log('HMR server successful started');
+  console.log('HMR server successfully started')
 })
-
-const compiler = webpack(webpackServerConfig);
 
 compiler.run((err) => {
   if (err) {
-    console.log('Compilation failed: ', err);
+    console.log('Compilation failed: ', err)
   }
-
   compiler.watch({}, (err) => {
     if (err) {
-      console.log('Compilation failed: ', err);
+      console.log('Compilation failed: ', err)
     }
     console.log('Compilation was successful')
   })
-
   nodemon({
     script: path.resolve(__dirname, '../dist/server/server.js'),
     watch: [
       path.resolve(__dirname, '../dist/server'),
-      path.resolve(__dirname, '../dist/client'), 
-    ]
+      path.resolve(__dirname, '../dist/client'),
+    ],
   })
 })
