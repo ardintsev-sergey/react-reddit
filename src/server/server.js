c5db1c7eef95d0bbf8099c2ac8ad24fa620d903a
Ardintsev Sergey <archunter1369@gmail.com>
2022-09-12 20:23:42 +0300

    fix redirect

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ app.use('/static', express.static('./dist/client'));
 app.get('/auth', (req, res) => {
   axios.post(
     'https://www.reddit.com/api/v1/access_token',
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=https://reddit-list.herokuapp.com/auth`,
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=https://react-reddit-app-archunter.herokuapp.com/auth`,
     {
       auth: {username: process.env.CLIENT_ID, password: process.env.SECRET},
       headers: {'Content-type': 'application/x-www-form-urlencoded'}
--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -12,7 +12,7 @@ interface IUserBlockProps {
 export function UserBlock({ avatarSrc, username, loading }: IUserBlockProps) {
     return (
         <a
-        href='https://www.reddit.com/api/v1/authorize?client_id=ETbnmE0Nvnz6m7rCR9gFUw&response_type=code&state=random_string&redirect_uri=https://reddit-list.herokuapp.com/auth&duration=permanent&scope=read submit identity'
+        href='https://www.reddit.com/api/v1/authorize?client_id=ETbnmE0Nvnz6m7rCR9gFUw&response_type=code&state=random_string&redirect_uri=https://react-reddit-app-archunter.herokuapp.com/auth&duration=permanent&scope=read submit identity'
         className={styles.userBox}>
             <div className={styles.avatarBox}>
               {avatarSrc
