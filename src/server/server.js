4bd4d34a4c3c7f25a5c86f83e6e0e76bac9f596d
Ardintsev Sergey <archunter1369@gmail.com>
2022-09-19 19:59:25 +0300

    error added

--- src/server/server.js
+++ src/server/server.js
@@ -27,19 +27,24 @@ app.use(helmet({
 app.use('/static', express.static('./dist/client'));
 
 app.get('/auth', (req, res) => {
-  axios.post(
-    'https://www.reddit.com/api/v1/access_token',
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${CURRENT_URL}`,
-    {
-      auth: {username: process.env.CLIENT_ID, password: process.env.SECRET},
-      headers: {'Content-type': 'application/x-www-form-urlencoded'}
-    }
-  )
-  .then(({ data }) => {
-    res.send(
-      indexHTMLTemplate(ReactDOM.renderToString(App()), data['access_token']),
-     );
-  });
+  axios
+    .post(
+      'https://www.reddit.com/api/v1/access_token',
+      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${CURRENT_URL}`,
+      {
+        auth: {username: process.env.CLIENT_ID, password: process.env.SECRET},
+        headers: {'Content-type': 'application/x-www-form-urlencoded'}
+      }
+    )
+    .then(({ data }) => {
+      res.send(
+        indexHTMLTemplate(ReactDOM.renderToString(App()), data['access_token']),
+      );
+    })
+    .catch((error) => {
+      console.log(error)
+      indexHTMLTemplate(ReactDOM.renderToString(App()), String(error.message));
+    });
 });
 
 app.get('*', (req, res) => {
