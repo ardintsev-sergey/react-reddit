44004b725da9bc6333c450bece49cdd4b1b2d1ee
Ardintsev Sergey <archunter1369@gmail.com>
2022-09-26 19:53:43 +0300

    userblock edited

--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -4,6 +4,7 @@ import { IconAnon } from '../../../Icons/IconAnon';
 import { EColor, Text } from '../../../Text/Text';
 import styles from './userBlock.css';
 // import { CLIENT_ID, CURRENT_URL } from '../../../../server/server';
+import {env} from 'process';
 interface IUserBlockProps {
     avatarSrc?: string
     username?: string
@@ -13,7 +14,7 @@ interface IUserBlockProps {
 export function UserBlock({ avatarSrc, username, loading }: IUserBlockProps) {
     return (
         <a
-        href={`https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${CURRENT_URL}&duration=temporary&scope=read submit identity`}
+        href={`https://www.reddit.com/api/v1/authorize?client_id=${env.CLIENT_ID}&response_type=code&state=random_string&redirect_uri=${env.CURRENT_URL}&duration=temporary&scope=read submit identity`}
         className={styles.userBox}>
             <div className={styles.avatarBox}>
               {avatarSrc
